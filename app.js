13cd42da4763ec7f18051ce6a5f292d292ed8731
aryanbati <aryanbatish496@gmail.com>
2024-11-18 10:52:45 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+
+// MADE BY ME ITS ARYAN BATISH 
+
+
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+//in the browser http://localhost:3000/user/23
+
+app.get("/user/:id", (req, res) => {
+   const userId = req.params.id;
+   res.send(`User id is ${userId}`);
+});
+
+app.listen(PORT, () => {
+   console.log(`Server started on ${PORT}`);
+});  
